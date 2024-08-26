2a30e67ae15ba9bd179ec7924bd9c9aee38cf73d
ary7012 <sofieyaaryssa@gmail.com>
2024-08-26 16:24:43 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED,
-getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
@@ -7,9 +6,9 @@ let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
-function main(curentTime) {
+function main(currentTime) {
   if (gameOver) {
-    if (confirm'(You lost. Press ok to restart.')) {
+    if (confirm('You lost. Press ok to restart.')) {
       window.location = '/'
     }
     return
@@ -17,9 +16,8 @@ function main(curentTime) {
 
 
   window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (curentTime - lastRenderTime) /
-1000
-  if (secondSinceLastRender < 1 / SNAKE_SPEED) return
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
 
   lastRenderTime = currentTime
@@ -27,6 +25,7 @@ function main(curentTime) {
   update()
   draw()
 }
+
 window.requestAnimationFrame(main)
 
 function update() {
@@ -35,7 +34,7 @@ function update() {
   checkDeath()
 }
 
-funtion draw () {
+function draw() {
   gameBoard.innerHTML = ''
   drawSnake(gameBoard)
   drawFood(gameBoard)
